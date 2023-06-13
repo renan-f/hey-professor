b2590fc5459f8de500ad086382ef759717d6ff00
Renan Andrade <filgueiras.rfa@gmail.com>
2023-06-13 14:09:40 -0300

    HEY-2: installing larastan, setting up and fixing initials errors

--- app/Http/Requests/ProfileUpdateRequest.php
+++ app/Http/Requests/ProfileUpdateRequest.php
@@ -5,18 +5,19 @@ namespace App\Http\Requests;
 use App\Models\User;
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Validation\Rule;
+use \Illuminate\Contracts\Validation\Rule as ContractRule;
 
 class ProfileUpdateRequest extends FormRequest
 {
     /**
      * Get the validation rules that apply to the request.
      *
-     * @return array<string, \Illuminate\Contracts\Validation\Rule|array|string>
+     * @return array<string, ContractRule|array<string|ContractRule>|string>
      */
     public function rules(): array
     {
         return [
-            'name'  => ['string', 'max:255'],
+            'name' => ['string', 'max:255'],
             'email' => ['email', 'max:255', Rule::unique(User::class)->ignore($this->user()->id)],
         ];
     }
