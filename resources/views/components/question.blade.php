c6a150e636c685f6b40770a1fe8046cd6526a7fc
Renan Andrade <filgueiras.rfa@gmail.com>
2023-06-20 14:02:46 -0300

    hey-12: being able to see the list of questions in the dashboard

---
+++ resources/views/components/question.blade.php
@@ -0,0 +1,4 @@
+@props(['question'])
+<div class="rounded dark:bg-gray-800/50 shadow shadow-blue-500/50 p-3 dark:text-gray-400">
+    {{ $question->question }}
+</div>
--- routes/web.php
+++ routes/web.php
@@ -1,21 +1,19 @@
 <?php
 
-use App\Http\Controllers\{ProfileController, QuestionController};
+use App\Http\Controllers\{DashboardController, ProfileController, QuestionController};
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     if (app()->isLocal()) {
         auth()->loginUsingId(1);
 
-        return view('dashboard');
+        return redirect('dashboard');
     }
 
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::get('/dashboard', DashboardController::class)->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::post('/question/store', [QuestionController::class, 'store'])->name('question.store');
 
